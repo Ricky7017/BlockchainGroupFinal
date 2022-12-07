9ac6b8aec470d6e234ac8a8d8d6521e5d07c4d4c
Ricky7017 <rickywalker7017@gmail.com>
2022-12-06 20:30:45 -0500

    Added flask routes and vote counter and vote checker

--- main1.py
+++ main1.py
@@ -70,3 +70,116 @@ class Blockchain(object):
     @property
     def last_block(self):
         return self.chain[-1]
+
+    # Counts all the votes that have occurred for each candidate
+    @property
+    def all_blocks(self):
+        temp = list(filter(lambda transactions: transactions['transactions'], self.chain))
+        temp2 = list(map(itemgetter('transactions'), temp))
+        vote1 = 0
+        vote2 = 0
+        for i in temp2:
+            vote = list(map(itemgetter('candidate_vote'), i))
+            vote = int(vote[0])
+            if vote == 1:
+                vote1 += 1
+            elif vote == 2:
+                vote2 += 1
+
+        return vote1, vote2
+
+    # Checks the voter ID to see if they already voted
+    @property
+    def check_vote_status(self):
+        temp = list(filter(lambda transactions: transactions['transactions'], self.chain))
+        temp2 = list(map(itemgetter('transactions'), temp))
+        for i in temp2:
+            voted = list(map(itemgetter('voter_ID'), i))
+            if voted == list(map(itemgetter('voter_ID'), i)):
+                print("you have already voted")
+                return True
+
+        return False
+
+
+# Flask , #routes
+app = Flask(__name__, template_folder="templates")
+blockchain = Blockchain()
+node_identifier = str(uuid4()).replace('-', "")
+
+
+# routes ####################################################################################
+@app.route('/blockchain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain)
+    }
+    return render_template("blockchain.html", data=response)
+
+
+@app.route('/', methods=['GET'])
+def vote_page():
+    return render_template("vote.html")
+
+
+@app.route('/candidate1', methods=['GET'])
+def add_vote_candidate1():
+    if not blockchain.check_vote_status:
+        blockchain.add_vote(
+            election="0",
+            voter_ID=node_identifier,
+            candidate_vote=1
+        )
+        last_block_hash = blockchain.Block_Hash(blockchain.last_block)
+        index = len(blockchain.chain)
+        nonce = blockchain.PoW(index, last_block_hash, blockchain.current_transaction)
+        block = blockchain.append_block(nonce, last_block_hash)
+        response = {
+            'message': "new block has been added",
+            'index': block['index'],
+            'hash_of_previous_block': block['Previous_block_hash'],
+            'nonce': block['nonce'],
+            'transaction': block['transactions']
+        }
+        return render_template("Candidate1vote.html"), response
+    else:
+        return render_template("AlreadyVoted.html")
+
+
+@app.route('/candidate2', methods=['GET'])
+def add_vote_candidate2():
+    if not blockchain.check_vote_status:
+        blockchain.add_vote(
+            election="0",
+            voter_ID=node_identifier,
+            candidate_vote=2
+        )
+        last_block_hash = blockchain.Block_Hash(blockchain.last_block)
+        index = len(blockchain.chain)
+        nonce = blockchain.PoW(index, last_block_hash, blockchain.current_transaction)
+        block = blockchain.append_block(nonce, last_block_hash)
+        response = {
+            'message': "new block has been added",
+            'index': block['index'],
+            'hash_of_previous_block': block['Previous_block_hash'],
+            'nonce': block['nonce'],
+            'transaction': block['transactions']
+        }
+        return render_template("Candidate2vote.html"), response
+    else:
+        return render_template("AlreadyVoted.html")
+
+
+@app.route('/results', methods=['GET'])
+def results():
+    vote1, vote2 = blockchain.all_blocks
+    response = {
+        'vote1': vote1,
+        'vote2': vote2
+    }
+    return render_template("results.html", data=response)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='127.0.0.1', port=int(8000))
